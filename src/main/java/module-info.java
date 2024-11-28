396158ccfe93d9c95a2744d7dcb9b36924ca4d91
Vu Cuong <hd.vucuong@gmail.com>
2024-11-28 21:26:55 +0700

    checkk

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,17 @@
 module org.example.librarymanagement1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
     requires com.google.gson;
     requires java.sql;
     requires java.dotenv;
-    requires javafx.graphics;
 
     opens org.example.librarymanagement1.frontend.Home to javafx.fxml;
     opens  org.example.librarymanagement1.frontend.Other to javafx.fxml;
     opens  org.example.librarymanagement1.frontend.SearchPage to javafx.fxml;
     opens org.example.librarymanagement1.frontend.BookManagementPage to javafx.fxml;
     opens org.example.librarymanagement1.frontend.BorrowMngPage to javafx.fxml;
+    opens org.example.librarymanagement1.frontend.UserMngPage to javafx.fxml;
     opens org.example.librarymanagement1 to javafx.fxml;
     exports org.example.librarymanagement1;
 }
